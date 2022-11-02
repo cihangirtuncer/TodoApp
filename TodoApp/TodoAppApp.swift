fda7faf9e6d4a848c0dbc4ad3ddab8a314d2d1f2
cihangirtuncer <cihangirtuncer184@gmail.com>
2022-11-02 03:42:35 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Cihangir Tuncer on 2.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
